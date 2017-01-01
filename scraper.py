2c97fb5e2fe61726b6a6e2f7ed14da8af2b71086
Christopher Hay <christopher.ray.hay@gmail.com>
2017-01-01 01:09:20 -0500

    reformatted

--- scraper.py
+++ scraper.py
@@ -8,7 +8,8 @@ class FreeFoodScraper(object):
         self.html_parser = HTMLParser()
         key = "4FC1A120A0BE8A7C90"
         netid = "chay"
-        url = "https://lists.princeton.edu/cgi-bin/wa?A1=ind{month_id}&L=freefood&X={key}&Y={netid}%40princeton.edu".format(month_id=month_id, key=key, netid=netid)
+        url = "https://lists.princeton.edu/cgi-bin/wa?A1=ind{month_id}&L=freefood&X={key}&Y={netid}%40princeton.edu".format(
+            month_id=month_id, key=key, netid=netid)
         curler = curl.Curl(url)
         body = curler.get()
         emails = re.findall("""<span onmouseover="showDesc\('.*?</span>""",
